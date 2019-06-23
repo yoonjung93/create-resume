50bf427626d0362c89d6db319b15c310a3d34367
Yoonjung Park <cute7483@gmail.com>
2019-06-23 22:03:00 +0900

    index page

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,102 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jquery: true
+  },
+  extends: ['eslint:recommended', 'airbnb'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+    ecmaVersion: 2018,
+    babelOptions: {
+      // configFile: '',
     },
-    // "extends": "eslint:recommended",
-    "extends": "airbnb",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
+  },
+  settings: {
+    'import/resolver': {
+      'webpack': {
+        'config': 'config/webpack.config.js'
+      }
     }
+  },
+  rules: {
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+    'prefer-arrow-callback': 'off',
+    'func-names': 'off',
+    'import/prefer-default-export': 'off',
+    'no-new-object': 'error',
+    'no-plusplus': 'off',
+    'brace-style': [
+      'warn',
+      'stroustrup', {
+        'allowSingleLine': true
+      }
+    ],
+    'no-array-constructor': 'error',
+    'no-inner-declarations': 0,
+    'space-before-function-paren': [
+      'error', {
+        'anonymous': 'always',
+        'named': 'never',
+        'asyncArrow': 'always'
+      }
+    ],
+    'dot-location': [
+      'error',
+      'property'
+    ],
+    'keyword-spacing': 'warn',
+    'space-before-blocks': [
+      'warn', {
+        'functions': 'always',
+        'keywords': 'always',
+        'classes': 'always'
+      }
+    ],
+    'indent': [
+      'error',
+      2,
+      {
+        'SwitchCase': 1
+      }
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'warn',
+      'single', {
+        'allowTemplateLiterals': true
+      }
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-unused-vars': 0,
+    'no-undef': 0,
+    'no-underscore-dangle': 0,
+    'no-empty': [
+      'error',
+      {
+        'allowEmptyCatch': true
+      }
+    ],
+    'no-debugger': 'error',
+    'no-alert': 'error',
+    'no-console': [
+      'warn',
+      {
+        'allow': [
+          'warn',
+          'error'
+        ]
+      }
+    ]
+  }
 };
--- src/App.js
+++ src/App.js
@@ -6,19 +6,65 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div>
+          <a
+            className="App-link"
+            href="/create"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Create
+          </a>
+        </div>
+        <div>
+          <a
+            className="App-link"
+            href="/design"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Design
+          </a>
+        </div>
+        <div>
+          <a
+            className="App-link"
+            href="/view"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View
+          </a>
+        </div>
+        <div>
+          <a
+            className="App-link"
+            href="/apply"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Apply
+          </a>
+        </div>
       </header>
+      <body className="App-body">
+        <code>src/App.js</code>
+      </body>
+      <footer className="App-footer">
+        <div>Yoonjung Park</div>
+        <div>email: cute7483@gmail.com</div>
+        <div>
+          <a
+            className="App-link"
+            href="https://yoonjung93.github.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Information
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
